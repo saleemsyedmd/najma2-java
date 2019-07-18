7a6c513a70f6d45c887de850c635e84168ab6dbc
moham <moham@DESKTOP-GT9TAGB>
2019-07-18 20:37:27 +0530

    writing a number writing a sentence

---
+++ Introduction/src/Java1/Addition.java
@@ -0,0 +1,17 @@
+package Java1;
+
+public class Addition {
+
+	public Addition() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		double number;
+		number = 4 ;
+				System.out.println(number);
+		
+		
+	}
+
+}
---
+++ Introduction/src/Java1/Sentence.java
@@ -0,0 +1,18 @@
+package Java1;
+
+public class Sentence {
+
+	public Sentence() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		double number;
+		number = 4 ;
+		System.out.print(" I  want ");
+		System.out.print(number);
+		System.out.println("Apples to eat .");
+
+	}
+
+}
